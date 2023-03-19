8d58388824718a5a44389f150613083b1b207046
Martin Chapman <contact@martinchapman.co.uk>
2023-03-19 21:07:45 +0000

    - Simplify content - Add press

--- js/js.js
+++ js/js.js
@@ -1,11 +1,3 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    for(let image of document.querySelectorAll('.section-transparent img')) {
-        image.addEventListener('mouseover', function (evt) {
-            evt.target.style.opacity = 0.5;
-        });
-        image.addEventListener('mouseout', function (evt) {
-            evt.target.style.opacity = 1;
-        });
-    };
 });
