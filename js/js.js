cf8a90d47259fee6cbfbacf26022d44d802179f2
Martin Chapman <contact@martinchapman.co.uk>
2023-03-22 22:29:25 +0000

    Add menu

--- js/js.js
+++ js/js.js
@@ -1,3 +1,10 @@
-
 document.addEventListener('DOMContentLoaded', function() {
+    for(let image of document.querySelectorAll('.image-links img')) {
+        image.addEventListener('mouseover', function (evt) {
+            evt.target.style.opacity = 0.5;
+        });
+        image.addEventListener('mouseout', function (evt) {
+            evt.target.style.opacity = 1;
+        });
+    };
 });
