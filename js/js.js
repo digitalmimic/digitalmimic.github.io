95727e0d3eaa1efa2d78f5503267228caae9eadc
Martin Chapman <contact@martinchapman.co.uk>
2023-03-26 13:11:09 +0100

    Squashed commit of the following:
    commit bdc45fa5de121e378541479bce4329e401280fd8
    Author: Martin Chapman <contact@martinchapman.co.uk>
    Date:   Sun Mar 26 13:10:53 2023 +0100
    
        Add graphs
    
    commit 67502b669f12906e793f6e38844ab18270c11ea0
    Author: Martin Chapman <contact@martinchapman.co.uk>
    Date:   Sat Mar 25 10:16:50 2023 +0000
    
        Add results image
    
    commit 07b43d67e1ee854c74abf8d94eaf2d92922894ac
    Author: Martin Chapman <contact@martinchapman.co.uk>
    Date:   Fri Mar 24 10:04:25 2023 +0000
    
        Error handling
    
    commit 3932050630718b4160ae94cac97271f3101017a7
    Author: Martin Chapman <contact@martinchapman.co.uk>
    Date:   Fri Mar 24 00:32:33 2023 +0000
    
        Reorder
    
    commit 4f9f1b388343ad9004fe14c69f7a711cb1c4721d
    Author: Martin Chapman <contact@martinchapman.co.uk>
    Date:   Fri Mar 24 00:14:30 2023 +0000
    
        Additional graph selections
    
    commit 7fbf3d813c42f1ed5aaabef2beab0b695c3da3ef
    Author: Martin Chapman <contact@martinchapman.co.uk>
    Date:   Thu Mar 23 17:59:37 2023 +0000
    
        Add additional graphs
    
    commit 09372aac25bdb84a20819cd698e2575671bfeb69
    Author: Martin Chapman <contact@martinchapman.co.uk>
    Date:   Thu Mar 23 16:01:18 2023 +0000
    
        Initial graph logic

--- js/js.js
+++ js/js.js
@@ -1,3 +1,35 @@
+function showGraph() {
+    if(document.getElementById('results-group').style.display=='none' || 
+    (document.getElementById('results-sec-container').style.display=='none' && document.getElementById('results-eth-container').style.display=='none') || 
+    document.getElementById('results-condition-container').style.display=='none' || 
+    document.getElementById('results-intervention-container').style.display=='none') {
+        return;
+    }
+
+    let graphString;
+    if(document.getElementById('results-intervention').value=="none") {
+        graphString = buildGraphString("sRandomWalk", ""); 
+    } else if(document.getElementById('results-intervention').value=="payment") {
+        graphString = buildGraphString("sRandomWalk", "VariableGas");
+    } else if(document.getElementById('results-intervention').value=="advice") {
+        graphString = buildGraphString("sMetaRandom", "");
+    } else if(document.getElementById('results-intervention').value=="vaccinations") {
+        graphString = buildGraphString("sRandomWalk", "VariableImmune");
+    }
+    document.getElementById('graph').innerHTML = graphString ? '<img class="graph center" src="images/graphs/' + graphString + '.png" onerror="document.getElementById(\'graph\').innerHTML=\'Please make another selection.\'; document.getElementById(\'results-context\').style.display = \'none\';" />' : document.getElementById('graph').innerHTML = '';
+    document.getElementById('results-context').style.display =  graphString ? 'inline' : 'none';
+}
+
+function buildGraphString(prefix, suffix) {
+    let seekers = [];
+    for(checkbox of document.querySelectorAll("* input[type='checkbox']")) {
+        if(checkbox.checked) seekers.push(prefix+checkbox.value+suffix);
+    }
+    seekers.sort();
+    if(seekers.length==0) return null;
+    return seekers.join('-') + '_' + document.getElementById('results-season').value;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     for(let image of document.querySelectorAll('.image-links img')) {
         image.addEventListener('mouseover', function (evt) {
@@ -7,4 +39,36 @@ document.addEventListener('DOMContentLoaded', function() {
             evt.target.style.opacity = 1;
         });
     };
+    document.getElementById('results-season').addEventListener('change', function (evt) {
+        showGraph();
+        document.getElementById('results-group').disabled = false;
+    });
+    document.getElementById('results-group').addEventListener('change', function (evt) {
+        showGraph();
+        if(evt.target.value=='sec') {
+            document.getElementById('results-eth-container').style.display = 'none';
+            for(let check of document.querySelectorAll("#results-eth-container input[type='checkbox']")) check.checked = false;
+            document.getElementById('results-sec-container').style.display = 'inline';
+            showGraph();
+        } else {
+            document.getElementById('results-sec-container').style.display = 'none';
+            for(let check of document.querySelectorAll("#results-sec-container input[type='checkbox']")) check.checked = false;
+            document.getElementById('results-eth-container').style.display = 'inline';
+            showGraph();
+        }
+        
+    }); 
+    for(check of document.querySelectorAll("* input[type='checkbox']")) {
+        check.addEventListener('click', function (evt) {
+            showGraph();
+            document.getElementById('results-subgroup').disabled = false;
+        });
+    };
+    document.getElementById('results-condition-container').addEventListener('change', function (evt) {
+        showGraph();
+        document.getElementById('results-intervention').disabled = false;
+    });
+    document.getElementById('results-intervention').addEventListener('change', function (evt) {
+        showGraph();
+    });
 });
